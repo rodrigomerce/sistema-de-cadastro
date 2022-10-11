53d74f8dc67690c20f4aaa03f03f7a05c7a1198a
Rodrigo Merce de Andrade Silva <RMAV@santosbrasil.com.br>
2022-10-11 19:42:15 -0300

    linkando o app js com o html

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ Para rodar esse projeto na sua máquina, por favor digite:
 
 ```
 node app.js
+
 ```
--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 console.log("Rodando o sistema de gerenciamente de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
+/* comentario */
